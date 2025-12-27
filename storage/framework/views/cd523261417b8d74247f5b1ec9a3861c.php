bdeec4ae1d2060ab56afae5a2d8dcbb37f02dbac
Moh Ali <moh23.m3@gmail.com>
2025-12-27 21:13:22 +0200

    Fixing map mage + setup the css files

--- storage/framework/views/cd523261417b8d74247f5b1ec9a3861c.php
+++ storage/framework/views/cd523261417b8d74247f5b1ec9a3861c.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="<?php echo e(csrf_token()); ?>">
-    <link rel="stylesheet" href="<?php echo e(asset('css/home.css')); ?>">
+    <link rel="stylesheet" href="<?php echo e(asset('css/app.css')); ?>">
     <link rel="stylesheet" href="<?php echo e(asset('css/notifications.css')); ?>">
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
